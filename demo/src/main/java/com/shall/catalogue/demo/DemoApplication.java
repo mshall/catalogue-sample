195e96e8e280e814346407eb2e65df9b74c8e779
Shall <mohamed.s.elshall2011@gmail.com>
2021-04-21 10:30:24 +0400

    Removed unnecessary logs from main class & will consider working on enhancements

--- demo/src/main/java/com/shall/catalogue/demo/DemoApplication.java
+++ demo/src/main/java/com/shall/catalogue/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.shall.catalogue.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,21 +10,15 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.shall.catalogue.demo.util.DataRetrievalUtil;
 import com.shall.catalogue.demo.util.DatabaseUtil;
 
 @SpringBootApplication
 //@EnableMongoRepositories(basePackageClasses = CatalogueRepositoryImpl.class)
 public class DemoApplication implements CommandLineRunner {
 
-	@Value("${mydatabase.name}")
-	private String databaseName;
-
-	@Autowired
-	DataRetrievalUtil networkUtil;
-
 	@Autowired
 	DatabaseUtil databaseUtil;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -41,8 +34,6 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Bean
 	public RestTemplate getRestTemplate() {
-		System.out.println("***********************************");
-		System.out.println("DemoApplication.getRestTemplate -> Start");
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
 		return restTemplate;
